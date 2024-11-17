fe2630ff9972497c1ba885363c46d744863d3aa9
umid1325 <u.mamasaliyev@newuu.uz>
2024-11-17 16:08:32 +0500

    changes

--- problem2.cpp
+++ problem2.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
-#include <math.h>
-#include <valarray>
 using namespace std;
 
 int main() {
     int m,n;
     cin>>m>>n;
-    int arr[m][n];
+    float arr[m][n];
     bool flag = 1;
     for(int i=0;i<m;i++) {
         for(int j=0;j<n;j++) {
@@ -19,19 +17,20 @@ int main() {
         for (int j = 0; j < n; j++) {
             sum += arr[i][j];
         }
-        avgs[i] += sum / n;
+        avgs[i] = sum / n;
     }
     for (int i = 0; i < n; i++) {
         float sum = 0;
         for (int j = 0; j < m; j++) {
             sum += arr[j][i];
         }
-        avgs[m+i] += sum / m;
+        avgs[m+i] = sum / m;
     }
     for(int i=0;i<m+n;i++) {
         if(avgs[0]!=avgs[i]) {
             flag = 0;
         }
+        cout<<avgs[i]<<" ";
     }
     if(flag) {
         cout<<"YES"<<endl;
